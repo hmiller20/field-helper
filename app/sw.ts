c612a48ae6ffc1e5d3193a7fa29c9480ddf898fe
hmiller20 <millerh113@gmail.com>
2025-06-27 21:59:21 -0400

    Update sw.ts

--- app/sw.ts
+++ app/sw.ts
@@ -1,7 +1,7 @@
 /// <reference lib="webworker" />
 
 import { Serwist } from "serwist";
-import { NetworkFirst } from "serwist";
+import { NetworkFirst, ExpirationPlugin } from "serwist";
 
 declare const self: ServiceWorkerGlobalScope & {
   __SW_MANIFEST: Array<string | { url: string; revision: string | null }>;
@@ -16,6 +16,13 @@ const serwist = new Serwist({
       matcher: ({ url }) => url.protocol.startsWith('http'),
       handler: new NetworkFirst({
         cacheName: "offlineCache",
+        plugins: [
+          new ExpirationPlugin({
+            maxEntries: 500, // Increase cache size
+            maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
+            purgeOnQuotaError: true, // Clean up if quota exceeded
+          }),
+        ],
       }),
     },
   ],

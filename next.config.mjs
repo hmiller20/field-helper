cc8c2047e0b41424a4587c45b8bc7eb5f6b6812e
hmiller20 <millerh113@gmail.com>
2025-09-25 08:13:14 -0400

    add gingerbread png to precache

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,7 @@ export default withSerwist({
   swSrc: 'public/sw.ts',
   swDest: 'public/sw.js',
   cacheOnNavigation: true,
-  additionalPrecacheEntries: ['/consent', 'manifest.json', 'favicon.ico', '/icon-192x192.png', '/icon-512x512.png', '/icon-any.svg'], // or '/' if you keep the root shell
+  additionalPrecacheEntries: ['/consent', 'manifest.json', 'favicon.ico', '/icon-192x192.png', '/icon-512x512.png', '/icon-any.svg', 'gingerbread.png'], // or '/' if you keep the root shell
   // register: true is default
   disable: process.env.NODE_ENV === 'development',
 })(nextConfig);

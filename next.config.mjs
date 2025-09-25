15f7379e19b2116c4fee2d41850bfdc95f92be52
hmiller20 <millerh113@gmail.com>
2025-09-25 19:38:20 -0400

    god damnint

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,11 @@
+import withSerwist from "@serwist/next";
+
 /** @type {import('next').NextConfig} */
-import withSerwistInit from "@serwist/next";
-
-const withSerwist = withSerwistInit({
-  swSrc: "app/sw.ts",
-  swDest: "public/sw.js",
-  disable: process.env.NODE_ENV === "development",
-});
-
 const nextConfig = {
-  // Your regular Next.js config options can go here
+  reactStrictMode: true,
 };
 
-export default withSerwist(nextConfig);
+export default withSerwist({
+  swSrc: "app/sw.ts",
+  swDest: "public/sw.js",
+})(nextConfig);
